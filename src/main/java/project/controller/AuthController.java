b9eff888f0cf8645caa9d932e66d29ad5c320b25
Jason Pan <jason.pan.us@gmail.com>
2024-11-29 23:21:34 -0800

    added unfinished dto->authresponse, loginrequest, registerrequest. AuthController, AuthenticationService. updated User.

---
+++ src/main/java/project/controller/AuthController.java
@@ -0,0 +1,30 @@
+package project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.dto.AuthResponse;
+import project.dto.LoginRequest;
+import project.dto.RegisterRequest;
+import project.service.AuthenticationService;
+
+// project/controller/AuthController.java
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
