498103e2631b3fb4d08c235cd4854ff11a56abad
Jason Pan <jason.pan.us@gmail.com>
2025-01-06 19:38:52 -0800

    Finished Basic security setup(Test passed)

--- src/main/java/project/controller/TestController.java
+++ src/main/java/project/controller/TestController.java
@@ -2,11 +2,16 @@ package project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
     @GetMapping("/test")
     public String test() {
         return "Hello from Spring Boot!";
+    }
+    @GetMapping("/hello")
+    public String protectedEndpoint() {
+        return "If you see this, authentication works!";
     }
 }
